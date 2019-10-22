4aacbf397288f76a4c27c1f92be5cd017403a1a7
2018ti <1285842658@qq.com>
2019-10-22 17:06:36 +0800

    ee

--- src/快速排序/quickSort.java
+++ src/快速排序/quickSort.java
@@ -6,6 +6,7 @@ import java.util.Random;
 
 public class quickSort {
     public static void main(String[] args) {
+        System.out.println("hahah");
         int [] a=new int[800000];
         for(int i=0;i<a.length;i++){
             a[i]= (int)(Math.random()*800000); //生成[0,800000)的随机数进行快排速度测试
